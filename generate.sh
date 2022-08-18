90b77d7e06c1a4cf4921e9ffbe64489f6924c0d2
alexanderyanin <alexander@ryanin.ru>
2022-08-18 16:35:55 +1000

    fist commit

---
+++ generate.sh
@@ -0,0 +1,26 @@
+echo "<!DOCTYPE html>" > index.html
+echo "<html>" >> index.html
+echo "<head>" >> index.html
+echo "<title>SrvStat</title>" >> index.html
+echo "<link rel="stylesheet" href="style.css">" >> index.html
+echo "</head>" >> index.html
+echo "<body>" >> index.html
+echo "<h1>" >> index.html
+uname -n >> index.html
+echo "</h1>" >> index.html
+echo "<p>Версия ядра: " >> index.html
+uname -r >> index.html
+echo "</p>" >> index.html
+echo "<p>" >> index.html
+uptime -p >> index.html
+echo "</p>" >> index.html
+echo "<p>Доступная память: " >> index.html
+free -h | grep Mem | awk '{print $2}' >> index.html
+echo "<br>Свободная память: " >> index.html
+free -h | grep Mem | awk '{print $4}' >> index.html
+echo "</p>" >> index.html
+
+
+
+echo "</body>" >> index.html
+echo "</html>" >> index.html
