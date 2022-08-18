448f6a1af7c0dbf43f931127a64b717a03f6719e
alexanderyanin <alexander@ryanin.ru>
2022-08-18 17:12:56 +1000

    storage info

--- generate.sh
+++ generate.sh
@@ -19,8 +19,13 @@ free -h | grep Mem | awk '{print $2}' >> index.html
 echo "<br>Свободная память: " >> index.html
 free -h | grep Mem | awk '{print $4}' >> index.html
 echo "</p>" >> index.html
-
-
+echo "<h3>Хранилище</h3><p>/: " >> index.html
+df -h | awk '$NF=="/"{printf "%s\t\t", $4}' >> index.html
+echo "<br>/mnt/one: " >> index.html
+df -h | awk '$NF=="/mnt/one"{printf "%s\t\t", $4}' >> index.html
+echo "<br>/mnt/two: " >> index.html
+df -h | awk '$NF=="/mnt/two"{printf "%s\t\t", $4}' >> index.html
+echo "</p>" >> index.html
 
 echo "</body>" >> index.html
 echo "</html>" >> index.html
